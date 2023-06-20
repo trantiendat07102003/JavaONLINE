8d1b88385dc2b49790e549cc5783d7d4292bbe92
trantiendat07102003 <trantiendatas@gmail.com>
2023-06-20 19:16:18 +0700

    java

--- Java_OOP_Fullhouse/src/java_oop_fullhouse/Developer.java
+++ Java_OOP_Fullhouse/src/java_oop_fullhouse/Developer.java
@@ -4,6 +4,66 @@ package java_oop_fullhouse;
  *
  * @author trant
  */
-public class Developer {
+public class Developer extends Employee {
+
+    private int expYear;
+    private String teamName;
+
+    public Developer() {
+    }
+
+    public Developer(int expYear, String teamName, int ID, String name, int age, int baseSalary) {
+        super(ID, name, age, baseSalary);
+        this.expYear = expYear;
+        this.teamName = teamName;
+    }
+
+    public int getExpYear() {
+        return expYear;
+    }
+
+    public void setExpYear(int expYear) {
+        this.expYear = expYear;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    @Override
+    public double getSalary() {
+
+        if (this.getExpYear() < 4) {
+            return this.getBaseSalary();
+        } else if (this.getExpYear() >= 4 && this.getExpYear() <= 7) {
+            return this.getBaseSalary() + (0.04 * this.getBaseSalary());
+        } else if (this.getExpYear() >= 8 && this.getExpYear() <= 10) {
+            return this.getBaseSalary() + (0.08 * this.getBaseSalary());
+        } else if (this.getExpYear() >= 11) {
+            return this.getBaseSalary() + (0.12 * this.getBaseSalary());
+        }
+
+        return 0;
+    }
+
+    @Override
+    public Employee copy() {
+
+        Employee employee = new Employee(expYear, teamName, expYear, expYear);
+
+        return employee;
+    }
     
+    @Override
+    public void printInformation(){
+        
+        System.out.printf("Developer[ ID = %d, Name = %s, Age = %d, BaseSalary = %d, ExpYear = %d, TeamName = %s]",
+                this.getID(),this.getName(),this.getAge(),this.getBaseSalary(),this.getExpYear(),this.getTeamName());
+        
+    }
+
 }
